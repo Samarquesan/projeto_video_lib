3ded0cd9e49847bb5019d3b36c0d4e7d01eb03f2
Samarquesan <sam.samarques28@gmail.com>
2025-06-02 22:39:46 -0300

    Implementação inicial dos componentes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
+
 import App from './App.jsx'
+import Home from './pages/Home.jsx'
+import Search from './pages/search.jsx'
+import Movies from './pages/Movies.jsx'
+
+import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route element={<App />}>
+          <Route path='/' element={<Home />} />
+          <Route path='movie/:id' element={<Movie />} />
+          <Route path='search' element={<Search />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
---
+++ src/pages/Movies.jsx
@@ -0,0 +1,7 @@
+const Movies = () => {
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Movies
