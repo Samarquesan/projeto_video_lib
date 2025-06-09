dbc3e6f64c4df83e260be60132d9a0aab7aded02
Samarquesan <sam.samarques28@gmail.com>
2025-06-09 17:24:45 -0300

    Atualização da pae home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,32 @@
+import { useState, useEffect } from "react";
+
+const moviesURL = import.meta.env.VITE_API;
+const apiKey = import.meta.env.VITE_API_Key;
+
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const [topMovies, setTopMovies] = useState([]);
+
+  const getTopRatedMovies = async (url) => {
+
+    const res = await fetch(url);
+    const data = await res.json();
+
+    setTopMovies(data.results);
+
+  };
+
+  useEffect(() => {
+
+    const topRatedUrl = `${moviesURL}top_rated?${apiKey}`;
+
+    console.log(topRatedUrl)
+
+  }, [])
+  
+  return <div>
+    {topMovies && topMovies.map(())}
+  </div>;
+  
+};
 
 export default Home
